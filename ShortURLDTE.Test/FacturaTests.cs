18be54d1251a747bec6a7064c45eded44df57358
Miguel Angel Vargas <miguelvargasp@gmail.com>
2025-04-10 04:10:03 -0400

    Primer commit para subir el proyecto ShortURLDTE de IConstruye

---
+++ ShortURLDTE.Test/FacturaTests.cs
@@ -0,0 +1,39 @@
+﻿using ShortURLDTE.Domain;
+using ShortURLDTE.Domain.Entities;
+using Xunit;
+
+namespace ShortURLDTE.Tests
+{
+    public class FacturaTests
+    {
+        [Fact]
+        public void Factura_PuedeSerConsultada_TresVeces()
+        {
+            // Arrange
+            var factura = new Factura();
+
+            // Act & Assert
+            Assert.True(factura.PuedeSerConsultada());
+            factura.RegistrarConsulta();
+
+            Assert.True(factura.PuedeSerConsultada());
+            factura.RegistrarConsulta();
+
+            Assert.True(factura.PuedeSerConsultada());
+            factura.RegistrarConsulta();
+
+            Assert.False(factura.PuedeSerConsultada());
+        }
+
+        [Fact]
+        public void Factura_NoPermiteMasDeTresConsultas()
+        {
+            var factura = new Factura();
+            factura.RegistrarConsulta();
+            factura.RegistrarConsulta();
+            factura.RegistrarConsulta();
+
+            Assert.Throws<InvalidOperationException>(() => factura.RegistrarConsulta());
+        }
+    }
+}
